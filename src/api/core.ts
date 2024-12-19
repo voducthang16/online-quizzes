b4bd7dda4611e058609d71c50fb26559fc9b767f
TK <voducthang16@gmail.com>
2024-12-19 21:56:54 +0700

    update core

--- src/api/core.ts
+++ src/api/core.ts
@@ -2,7 +2,7 @@ import { ApiRequestModel, ApiResponseModel } from '@/models';
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 const api: AxiosInstance = axios.create({
-    baseURL: 'https://c9d1-125-235-239-241.ngrok-free.app/api/',
+    baseURL: 'https://78ab-125-235-239-241.ngrok-free.app/api/',
     headers: {
         'Content-Type': 'application/json',
     },
